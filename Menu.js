ec394c426830a3397f27b708b619e2c8be6d4041
JoaoFranco23 <147751659+JoaoFranco23@users.noreply.github.com>
2024-01-22 23:50:17 -0300

    Create Menu.js

---
+++ Menu.js
@@ -0,0 +1,7 @@
+function clickMenu(){
+    if (menu_dropdown.style.display == 'block'){
+        menu_dropdown.style.display = 'none';
+    } else {
+        menu_dropdown.style.display = 'block';
+    } 
+}
